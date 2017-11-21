e8b0e89d0fb9dd933a2c2e2ccdcd55c99321342e
jairdean <jairdean@hotmail.com>
2017-11-21 12:49:39 -0500

    login version jairo

---
+++ src/java/com/sppp/beans/Perfil.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sppp.beans;
+
+import java.util.Set;
+/**
+ *
+ * @author Jairo
+ */
+public class Perfil {
+
+  private int id_tbperfil;
+  private String descripcion;
+  private Set<Usuario> usuarios;
+
+    public Set<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Set<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public int getId_tbperfil() {
+        return id_tbperfil;
+    }
+
+    public void setId_tbperfil(int id_tbperfil) {
+        this.id_tbperfil = id_tbperfil;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+   
+  
+  
+}
---
+++ src/java/com/sppp/beans/Usuario.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sppp.beans;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Jairo
+ */
+@ManagedBean
+public class Usuario {
+  private String id_cedula;
+  private String correo;
+  private String clave;
+  private String nombre;
+  private String apellido;
+  private String telefono;
+  private String direccion;
+  private Perfil perfil;
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+  private boolean estado;
+
+    public String getId_cedula() {
+        return id_cedula;
+    }
+
+    public void setId_cedula(String id_cedula) {
+        this.id_cedula = id_cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+  
+  
+}

e7d834748eb1fd871803b3f7c0cdfb92561ab514
JCastillo92 <jhon_j92@hotmail.com>
2017-11-22 17:33:48 -0500

    Filtro X Roles

--- src/java/com/sppp/utils/LoginFilter.java
+++ src/java/com/sppp/utils/LoginFilter.java
@@ -26,40 +26,76 @@ import javax.servlet.http.HttpSession;
  */
 @WebFilter(filterName = "LoginFilter", urlPatterns = {"/*"})
 public class LoginFilter implements Filter {
-    
+
     public LoginFilter() {
-	}
+    }
 
-	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
 
-	}
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
-		try {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        try {
 
-			HttpServletRequest reqt = (HttpServletRequest) request;
-			HttpServletResponse resp = (HttpServletResponse) response;
-			HttpSession ses = reqt.getSession(false);
+            HttpServletRequest reqt = (HttpServletRequest) request;
+            HttpServletResponse resp = (HttpServletResponse) response;
+            HttpSession ses = reqt.getSession(false);
 
-			String reqURI = reqt.getRequestURI();
-			if (reqURI.indexOf("/login.xhtml") >= 0
-					|| (ses != null && ses.getAttribute("username") != null)
-					|| reqURI.indexOf("/public/") >= 0
-					|| reqURI.contains("javax.faces.resource"))
-				chain.doFilter(request, response);
-			else
-				resp.sendRedirect(reqt.getContextPath() + "/faces/login.xhtml");
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+            String reqURI = reqt.getRequestURI();
+            if (reqURI.indexOf("/login.xhtml") >= 0
+                    || (ses != null && ses.getAttribute("username") != null)
+                    || reqURI.indexOf("/public/") >= 0
+                    || reqURI.contains("javax.faces.resource")) {
+                
+                //chain.doFilter(request, response);
+                
+                //BLOQUEADOR DE PAGINAS
+                int perfil = (int) ses.getAttribute("perfil");
+                if(perfil == 1){
+                    //Para acceder a paginas de estudiante o publicas
+                    if(reqURI.indexOf("/user/estudiantes") >= 0 || reqURI.indexOf("/public/") >=0){
+                        
+                        //No debe volver al login
+                        if (reqURI.indexOf("/login.xhtml") >= 0){
+                            resp.sendRedirect(reqt.getContextPath() + "/faces/user/estudiantes/student.xhtml");
+                        }else{
+                            chain.doFilter(request, response);
+                        }
+                    }
+                } // FIN estudiante
+                if(perfil == 2){
+                    //Para acceder a paginas de estudiante o publicas
+                    if(reqURI.indexOf("/user/gestores") >= 0 || reqURI.indexOf("/public/") >=0){
+                        
+                        //No debe volver al login
+                        if (reqURI.indexOf("/login.xhtml") >= 0){
+                            resp.sendRedirect(reqt.getContextPath() + "/faces/user/gestores/attendant.xhtml");
+                        }else{
+                            chain.doFilter(request, response);
+                        }
+                    }
+                } // FIN estudiante
+                else{
+                    if(ses == null){
+                        chain.doFilter(request, response);
+                    }
+                }
+                
+                
+            } else {
+                resp.sendRedirect(reqt.getContextPath() + "/faces/login.xhtml");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-	@Override
-	public void destroy() {
+    @Override
+    public void destroy() {
 
-	}
-    
+    }
+
 }

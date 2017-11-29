d62ddf4df72e01c3c3d57be1a8c7d632948425bf
jairdean <jairdean@hotmail.com>
2017-11-29 15:51:10 -0500

    creacion hbm de preguntas y union con respuestas

---
+++ src/java/com/sppp/beans/Preguntas.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sppp.beans;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Jairo
+ */
+public class Preguntas {
+    private long id_tbpreguntas;
+    private String descripcion;
+    private boolean estado_preguntas;
+    private Set<Respuesta> respuesta;
+
+    public long getId_tbpreguntas() {
+        return id_tbpreguntas;
+    }
+
+    public void setId_tbpreguntas(long id_tbpreguntas) {
+        this.id_tbpreguntas = id_tbpreguntas;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado_preguntas() {
+        return estado_preguntas;
+    }
+
+    public void setEstado_preguntas(boolean estado_preguntas) {
+        this.estado_preguntas = estado_preguntas;
+    }
+
+    public Set<Respuesta> getRespuesta() {
+        return respuesta;
+    }
+
+    public void setRespuesta(Set<Respuesta> respuesta) {
+        this.respuesta = respuesta;
+    }
+    
+    
+}

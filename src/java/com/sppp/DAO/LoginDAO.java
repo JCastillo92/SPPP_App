23c2e6f0137c8f19d3162b6690bebf5104be0b93
DidiAndy <bryans71@live.com>
2017-12-20 13:12:42 -0500

    query of war

--- src/java/com/sppp/DAO/LoginDAO.java
+++ src/java/com/sppp/DAO/LoginDAO.java
@@ -31,16 +31,17 @@ public class LoginDAO {
         Usuario usuario=null;
         try {
             tx = sesion.beginTransaction();
-       /* AlmacenamientoPDF obj=new AlmacenamientoPDF();
-        MailingMain obj2=new MailingMain();
-        obj2.deliverMain();
-            //obj.create_student_folder_first_time(123);
+            AlmacenamientoPDF obj=new AlmacenamientoPDF();
+        
+            obj.create_student_folder_first_time(123);
             obj.guardado_archivo_pdf_creado(123,101);
             obj.guardado_archivo_pdf_creado(123,102);
             
             obj.guardado_archivo_pdf_creado(123,104);
-       */
+       
             /*
+            MailingMain obj2=new MailingMain();
+        obj2.deliverMain();
                 
             
                       AlmacenamientoPDF obj=new AlmacenamientoPDF();

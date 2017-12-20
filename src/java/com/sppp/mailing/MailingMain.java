76cf388417ad2dd3741057f5e11fd6482c7ef29d
jairdean <jairdean@hotmail.com>
2017-12-20 14:22:14 -0500

    cambios mailing

--- src/java/com/sppp/mailing/MailingMain.java
+++ src/java/com/sppp/mailing/MailingMain.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+https://www.youtube.com/watch?v=D-NYmDWiFjU
  */
 package com.sppp.mailing;
 import java.util.Properties;

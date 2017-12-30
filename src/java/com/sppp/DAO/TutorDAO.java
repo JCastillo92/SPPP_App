a9b953804080304dda50f579cb322f28604680ce
jairdean <jairdean@hotmail.com>
2017-12-30 13:05:25 -0500

    backup

--- src/java/com/sppp/DAO/TutorDAO.java
+++ src/java/com/sppp/DAO/TutorDAO.java
@@ -6,24 +6,27 @@
 package com.sppp.DAO;
 
 import com.sppp.beans.Tutor;
+import com.sppp.utils.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  *
  * @author Jairo
  */
 public class TutorDAO {
-        public Tutor findProceso(long id){
-        /*
-        Tutor proc = new Tutor();
+    /*
+        public Tutor findTutorVisita(){
+        Tutor datoTutor = new Tutor();
         SessionFactory sf=HibernateUtil.getSessionFactory();
         Session sesion=sf.openSession();
         Transaction tx=null;
-        
         try{
             tx = sesion.beginTransaction();
-            Query query = sesion.createQuery(" from Proceso WHERE id_proceso = :id ");
-            query.setLong("id", id);
-            proc = (Proceso) query.uniqueResult();
+            Query query = sesion.createQuery(" from Tutor WHERE id_proceso = :id ");
+            datoTutor = (Tutor) query.uniqueResult();
             tx.commit();
         }catch (Exception e) {
             e.printStackTrace();
@@ -35,9 +38,8 @@ public class TutorDAO {
             //para cerrar seesion
             sesion.close();
         }
-        
-        return proc;
-*/
-        return null;
+        return datoTutor;
+
     }
+*/
 }
